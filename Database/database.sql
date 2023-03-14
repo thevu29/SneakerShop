ca8a93fed4018c8e863b9304ba619adc93d5950f
thevu29 <vnguyen132az@gmail.com>
2023-03-14 10:59:34 +0700

    commit

--- Database/database.sql
+++ Database/database.sql
@@ -1,153 +1,141 @@
 ﻿create database py_ql
 use py_ql
 
-create table ACCESS
+create table Access
 (
-	AccsID varchar(8) primary key,
-	AccsName nvarchar(10),
-	AccsDescription nvarchar(30)
+	AccessID varchar(8) primary key,
+	AccesssName nvarchar(25),
+	AccesssDescription nvarchar(30)
 )
-create table ACCOUNT
+
+create table Account
 (
-	AccID varchar(8) primary key,
-	UserName varchar(12),
-	Pass varchar(15),
-	AccsID varchar(8),
-	foreign key (AccsID) references ACCESS(AccsID)
+	AccountID varchar(8) primary key,
+	Username varchar(12),
+	Password varchar(15),
+	AccessID varchar(8),
+	foreign key (AccessID) references Access(AccessID)
 )
-create table CUSTOMER
+
+create table Customer
 (
-	CustID varchar(8) primary key,
-	CustName nvarchar(15),
-	CustAddress nvarchar(12),
+	CustomerID varchar(8) primary key,
+	CustomerName nvarchar(50),
+	CustomerAddress nvarchar(12),
 	Phone varchar(12),
-	Sex nvarchar(5),
-	Point int,
-	AccID varchar(8),
-	foreign key (AccID) references ACCOUNT(AccID)
+	Gender nvarchar(5),
+	Point int, 
+	AccountID varchar(8),
+	foreign key (AccountID) references Account(AccountID)
 )
-create table PURCHASE_ORDER
+
+create table CustomerOrder
 (
 	OrderID varchar(8) primary key,
-	CustID varchar(8),
-	OrderDate datetime,
-	Total int,
-	Order_Status nvarchar(10),
-	foreign key (CustID) references CUSTOMER(CustID)
+	CustomerID varchar(8),
+	OrderDate datetime, 
+	TotalPrice float, 
+	OrderStatus nvarchar(10),
+	foreign key (CustomerID) references Customer(CustomerID)
 )
-create table SUPPLIER
+
+create table Supplier
 (
-	SuppID varchar(8) primary key,
-	SuppName nvarchar(20),
-	SuppAddress nvarchar(20),
+	SupplierID varchar(8) primary key,
+	SupplierName nvarchar(50),
+	SupplierAddress nvarchar(20),
 	Phone varchar(12),
 	Email varchar(15)
 )
-create table CATEGORY
+create table Category
 (
-	CatgID varchar(8) primary key,
-	CatgName nvarchar(20),
-	CatgDescription nvarchar(30)
+	CategoryID varchar(8) primary key,
+	CategoryName nvarchar(50),
 )
-create table PRODUCT
+
+create table Product
 (
-	ProdID varchar(8) primary key,
-	ProdName nvarchar(20),
-	Price money,
-	Stocks int,
-	Category nvarchar(15),
-	SuppID varchar(8),
-	CatgID varchar(8),
-	ImgLink varchar(50),
-	foreign key (CatgID) references CATEGORY(CatgID),
-	foreign key (SuppID) references SUPPLIER(SuppID)
+	ProductID varchar(8) primary key,
+	ProductName nvarchar(50),
+	Price money, 
+	Quantity int, 
+	SupplierID varchar(8),
+	CategoryID varchar(8),
+	ImageLink varchar(50),
+	foreign key (CategoryID) references Category(CategoryID),
+	foreign key (SupplierID) references Supplier(SupplierID)
 )
-create table ORDERDETAIL
+
+create table OrderDetail
 (
 	OrderID varchar(8),
-	ProdID varchar(8),
-	Quantity int,
-	primary key (OrderID, ProdID),
-	foreign key (OrderID) references PURCHASE_ORDER(OrderID),
-	foreign key (ProdID) references PRODUCT(ProdID)
+	ProductID varchar(8),
+	Quantity int, 
+    Price float, 
+	primary key (OrderID,  ProductID),
+	foreign key (OrderID) references CustomerOrder(OrderID),
+	foreign key (ProductID) references Product(ProductID)
 )
-INSERT INTO ACCESS VALUES
-('ACC111','nv1','description1'),
-('ACC112','nv2','description2'),
-('ACC113','nv3','description3'),
-('ACC114','nv4','description4'),
-('ACC115','nv5','description5'),
-('ACC116','nv6','description6'),
-('ACC117','nv7','description7')
 
-INSERT INTO ACCOUNT VALUES
-('AC111','nhanvien1','111','ACC111'),
-('AC112','nhanvien2','112','ACC112'),
-('AC113','nhanvien3','113','ACC113'),
-('AC114','nhanvien4','114','ACC114'),
-('AC115','nhanvien5','115','ACC115'),
-('AC116','nhanvien6','116','ACC116'),
-('AC117','nhanvien7','117','ACC117')
+INSERT INTO Access VALUES
+('ACS001', 'Admin', N'Người quản trị'),
+('ACS002', 'User', N'Người dùng')
 
-INSERT INTO CUSTOMER VALUES
-('CUS111','a',N'Hồ Chí Minh','0112233441','nam',10,'AC111'),
-('CUS112','b',N'Hà Nội','0112233442','nữ',10,'AC112'),
-('CUS113','c',N'Đồng Nai','0112233443','nữ',10,'AC113'),
-('CUS114','d',N'Quảng Bình ','0112233444','nam',10,'AC114'),
-('CUS115','e',N'Hồ Chí Minh','0112233445','nam',10,'AC115'),
-('CUS116','f',N'Đồng Nai','0112233446','nam',10,'AC116'),
-('CUS117','g',N'Khánh Hòa','0112233447','nữ',10,'AC117')
+INSERT INTO Account VALUES
+('ACC001', 'admin', 'admin', 'ACS001'),
+('ACC002', 'thevu', '123', 'ACS002'),
+('ACC003', 'huyhoang', '123', 'ACS002'),
+('ACC004', 'kimphu', '123', 'ACS002'),
+('ACC005', 'hainam', '123', 'ACS002'),
+('ACC006', 'ducthang', '123', 'ACS002')
 
-INSERT INTO PURCHASE_ORDER VALUES
-('ORDERID1','CUS111','2023-1-1',12,'status1'),
-('ORDERID2','CUS112','2023-1-2',11,'status2'),
-('ORDERID3','CUS113','2023-1-3',10,'status3'),
-('ORDERID4','CUS114','2023-1-4',9,'status4'),
-('ORDERID5','CUS115','2023-1-5',5,'status5'),
-('ORDERID6','CUS116','2023-1-6',18,'status6'),
-('ORDERID7','CUS117','2023-1-7',4,'status7')
+INSERT INTO Customer VALUES
+('CUS001', N'Nguyễn Thế Vũ', N'Hồ Chí Minh', '0123456789', 'Nam', 10, 'ACC002'),
+('CUS002', N'Vương Huy Hoàng', N'Hà Nội', '0123456789', N'Nữ', 10, 'ACC003'),
+('CUS003', N'Nguyễn Hoàng Hải Nam', N'Đồng Nai', '0123456789', N'Nữ', 10, 'ACC004'),
+('CUS004', N'Trần Kim Phú', N'Quảng Bình ', '0123456789', N'Nam', 10, 'ACC005'),
+('CUS005', N'Đào Đức Thắng', N'Hồ Chí Minh', '0123456789', N'Nam', 10, 'ACC006')
 
-INSERT INTO SUPPLIER VALUES
-('IDS111','a',N'Hồ Chí Minh 1','0112233441','a@gmail.com'),
-('IDS112','b',N'Hồ Chí Minh 2','0112233442','b@gmail.com'),
-('IDS113','c',N'Hồ Chí Minh 3','0112233443','c@gmail.com'),
-('IDS114','d',N'Hồ Chí Minh 4','0112233444','d@gmail.com'),
-('IDS115','e',N'Hồ Chí Minh 5','0112233445','e@gmail.com'),
-('IDS116','f',N'Hồ Chí Minh 6','0112233446','f@gmail.com'),
-('IDS117','g',N'Hồ Chí Minh 7','0112233447','g@gmail.com')
+INSERT INTO CustomerOrder VALUES
+('HD001', 'CUS001', '2023-1-1', 12, 'Đã xử lí'),
+('HD002', 'CUS002', '2023-1-2', 11, 'Chưa xử lí'),
+('HD003', 'CUS003', '2023-1-3', 10, 'Đã xử lí'),
+('HD004', 'CUS004', '2023-1-4', 9, 'Đã xử lí'),
+('HD005', 'CUS005', '2023-1-5', 5, 'Chưa xử lí')
 
-INSERT INTO CATEGORY VALUES
-('CA111','name1','description1'),
-('CA112','name2','description2'),
-('CA113','name3','description3'),
-('CA114','name4','description4'),
-('CA115','name5','description5'),
-('CA116','name6','description6'),
-('CA117','name7','description7')
+INSERT INTO Supplier VALUES
+('SPL001', 'NIKE', N'Hồ Chí Minh', '0123456789', 'nike@gmail.com'),
+('SPL002', 'ADDIDAS', N'Hà Nội', '0123456789', 'addidas@gmail.com'),
+('SPL003', 'BALENCIAGA', N'Đà Nẵng', '0123456789', 'balenciaga@gmail.com'),
+('SPL004', 'GUCCI', N'Nha Trang', '0123456789', 'gucci@gmail.com'),
+('SPL005', 'Vans', N'Hồ Chí Minh', '0123456789', 'vans@gmail.com'),
+('SPL006', 'Converse', N'Hà Nội', '0123456789', 'converse@gmail.com'),
+('SPL007', 'Jordan', N'Đà Nẵng', '0123456789', 'jordan@gmail.com')
 
-INSERT INTO PRODUCT VALUES
-('MSP111','VANS VAULT',1100000,36,'Vans','IDS111','CA111','link1'),
-('MSP112','Converse Chuck',1200000,37,'Converse','IDS112','CA112','link2'),
-('MSP113','Air Jordan 1 Retro',1300000,38,'Nike','IDS113','CA113','link3'),
-('MSP114','ULTRA 4DFWD SHOES',1400000,39,'Adidas','IDS114','CA114','link4'),
-('MSP115','BASAS WORKADAY',1500000,40,'Ananas','IDS115','CA115','link5'),
-('MSP116','Nike Air Max',1600000,41,'Nike','IDS116','CA116','link6'),
-('MSP117','Chunky Liner Yankees',1700000,42,'MLB','IDS117','CA117','link7')
+INSERT INTO Category VALUES
+('CAT001', 'Nike'),
+('CAT002', 'Addidas'),
+('CAT003', 'Balenciaga'),
+('CAT004', 'Gucci'),
+('CAT005', 'Vans'),
+('CAT006', 'Converse'),
+('CAT007', 'Jordan')
 
-INSERT INTO ORDERDETAIL VALUES
-('ORDERID1','MSP111',11),
-('ORDERID2','MSP112',12),
-('ORDERID3','MSP113',13),
-('ORDERID4','MSP114',14),
-('ORDERID5','MSP115',15),
-('ORDERID6','MSP116',16),
-('ORDERID7','MSP117',17)
+INSERT INTO Product VALUES
+('SP001', 'Vans Vault', 1100000, 50, 'SPL005', 'CAT005', 'link1'),
+('SP002', 'Converse Chuck', 1200000, 50, 'SPL006', 'CAT006', 'link2'),
+('SP003', 'Air Jordan 1 Retro', 1300000, 50, 'SPL007', 'CAT007', 'link3'),
+('SP004', 'Nike Air Force 1', 1400000, 50, 'SPL001', 'CAT001', 'link4'),
+('SP005', 'Ultra Addidas 4D', 1500000, 50, 'SPL002', 'CAT002', 'link5'),
+('SP006', 'Nike Air Max', 1600000, 50, 'SPL001', 'CAT001', 'link6'),
+('SP007', 'Balenciaga Stan Smith', 1700000, 50, 'SPL003', 'CAT003', 'link7'),
+('SP008', 'Balenciaga Stan Smith', 1700000, 50, 'SPL003', 'CAT003', 'link7'),
+('SP009', 'Balenciaga Stan Smith', 1700000, 50, 'SPL003', 'CAT003', 'link7'),
+('SP010', 'Balenciaga Stan Smith', 1700000, 50, 'SPL003', 'CAT003', 'link7')
 
-
-
-
-
-
-
-
-
+INSERT INTO OrderDetail VALUES
+('HD001', 'SP001', 5, 1100000),
+('HD002', 'SP002', 1, 1200000),
+('HD003', 'SP003', 2, 1300000),
+('HD004', 'SP004', 10, 1400000),
+('HD005', 'SP005', 5, 1500000)
